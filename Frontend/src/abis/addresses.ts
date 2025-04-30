d3e153155ea19884f5b40fe9741ded06b8e25d5d
JamesVictor-o <victorjames40v@gmail.com>
2025-04-30 15:10:15 +0100

    ...

--- Frontend/src/abis/addresses.ts
+++ Frontend/src/abis/addresses.ts
@@ -1,4 +1,4 @@
-export const FairPayCore = "0x357eD459877268d7F668d714e6Fa79b9B206e48B";
+export const FairPayCore = "0x4813CA154F7CE5833585664310D0eD1F69F1ADFe";
 export const FeesManager = "0xD3DcDAD68d693ce68e4Ce9e85E3661B690Da5A67";
 export const OrganizationManager = "0x76fB8A29822545bd10ad1132c5440Fa59fE0701a";
 export const WorkerDashboard = "0x58B7bA011AbFBc017Dc62681e4195D2bE5A6a244";
